086d84fc9a2698e9961821991a9c6b42cb34af8a
Swarnabha Ghosh <swarnabhaghosh2005@gmail.com>
2025-11-20 10:32:06 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-// Store all books here
 const myLibrary = [];
 
 // Book constructor
-function Book(title, author, pages, hasRead) {
-  this.id = crypto.randomUUID(); // unique stable ID
+function Book(title, author, pages, read) {
+  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.hasRead = hasRead;
-
-  this.info = function () {
-    const readStatus = this.hasRead ? "already read" : "not read yet";
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
-  };
+  this.read = read;
 }
 
-// Function to add a new book to the library
-function addBookToLibrary(title, author, pages, hasRead) {
-  const newBook = new Book(title, author, pages, hasRead);
+// Add a book to the array
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-  return newBook; 
+  displayBooks();  // refresh UI after adding
+}
+
+// Display all books on the page
+function displayBooks() {
+  const container = document.getElementById("library-display");
+  container.innerHTML = ""; // clear before redisplay
+
+  myLibrary.forEach((book) => {
+    // Create card
+    const card = document.createElement("div");
+    card.classList.add("book-card");
+
+    card.innerHTML = `
+      <h3>${book.title}</h3>
+      <p><strong>Author:</strong> ${book.author}</p>
+      <p><strong>Pages:</strong> ${book.pages}</p>
+      <p><strong>Read:</strong> ${book.read ? "Yes" : "No"}</p>
+      <p><small>ID: ${book.id}</small></p>
+    `;
+
+    container.appendChild(card);
+  });
 }
 
